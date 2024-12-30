4a7302f20e5031b61d228ea43ca2d4626851abf5
sonln99 <sonln4793@gmail.com>
2024-12-30 15:28:56 +0700

    update

--- components/chat-box/index.tsx
+++ components/chat-box/index.tsx
@@ -1,18 +1,14 @@
 "use client";
-import Link from "next/link";
 import { useEffect, useRef, useState } from "react";
 import InputGroup from "../input-group";
 import { IChat } from "@/app/types/types";
 import { useWallet } from "@solana/wallet-adapter-react";
 import { useSQRAI } from "@/app/provider/sqrai.provider";
 import { BotReply, useBotAutoReply } from "@/app/serivces/bot-api";
-import utils from "@/app/utils";
-import Image from "next/image";
 import ReactMarkdown from "react-markdown";
 let intervalId;
 const ChatBox = () => {
   const { publicKey } = useWallet();
-  const [avatar, setAvatar] = useState<string>("");
   const messagesEndRef = useRef<HTMLInputElement>(null);
   const { sessionContent, setSessionContent, sessionId, dataChat } = useSQRAI();
   const [isLoading, setLoading] = useState<boolean>(false);
@@ -78,12 +74,6 @@ const ChatBox = () => {
     messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
   };
 
-  useEffect(() => {
-    if (publicKey?.toString()) {
-      setAvatar(utils.genAVT(publicKey?.toString() as string));
-    }
-  }, [publicKey?.toString()]);
-
   return (
     <div className="flex flex-col h-full w-full border-l border-[#a4fb0e] bg-black">
       <div className="grow overflow-auto transition-all p-5">
@@ -92,22 +82,8 @@ const ChatBox = () => {
             {item.from !== "bot" && (
               <div key={index}>
                 <div className="flex flex-col items-start gap-0 mb-4">
-                  {/* <div className="flex gap-1 p-1 border border-[#a4fb0e] overflow-hidden"> */}
-                  {/* <img
-                      className="w-6 h-6 rounded-full"
-                      src={avatar}
-                      alt={publicKey?.toString()}
-                    /> */}
                   <div className="flex items-center space-x-2 rtl:space-x-reverse">
                     <div className="text-sm font-bold text-[#a4fb0e] font-bricolage flex">
-                      {/* {publicKey
-                          ? ` ..${publicKey
-                              ?.toString()
-                              ?.substring(
-                                publicKey?.toString().length - 6,
-                                publicKey?.toString().length
-                              )}`
-                          : "User"} */}
                       &gt;_ You
                     </div>
                   </div>
@@ -117,7 +93,6 @@ const ChatBox = () => {
                       <span style={{ whiteSpace: "pre-line" }}>
                         {item.value}
                       </span>
-                      {/* <ReactMarkdown>{item.value}</ReactMarkdown> */}
                     </div>
                   </div>
                 </div>
@@ -126,20 +101,11 @@ const ChatBox = () => {
             {item.from == "bot" && (
               <div key={index}>
                 <div className="flex flex-col items-start gap-0 mb-4 ">
-                  {/* <div className="flex gap-2 items-center justify-center p-1 border border-[#a4fb0e] overflow-hidden"> */}
-                  {/* <Image
-                      src={"/imgs/sqr-logo.svg"}
-                      className=""
-                      alt={""}
-                      width={50}
-                      height={30}
-                    ></Image> */}
                   <div className="flex items-center space-x-2 rtl:space-x-reverse">
                     <span className="text-sm font-bold text-white font-bricolage">
                       &gt;_ beta
                     </span>
                   </div>
-                  {/* </div> */}
                   <div className="flex flex-col">
                     <div className="text-sm font-normal break-all text-white">
                       <ReactMarkdown>{item.value}</ReactMarkdown>
@@ -153,18 +119,10 @@ const ChatBox = () => {
 
         {isLoading && (
           <div className="flex flex-col items-start gap-2 mb-4">
-            <div className="flex gap-2 items-center justify-center p-1 border border-[#a4fb0e] overflow-hidden">
-              {/* <Image
-                src={"/imgs/sqr-logo.svg"}
-                alt={""}
-                width={50}
-                height={30}
-              ></Image> */}
-              <div className="flex items-center space-x-2 rtl:space-x-reverse">
-                <span className="text-sm font-semibold text-[#a4fb0e] font-bricolage">
-                  B-a3cs4
-                </span>
-              </div>
+            <div className="flex items-center space-x-2 rtl:space-x-reverse">
+              <span className="text-sm font-bold text-white font-bricolage">
+                &gt;_ beta
+              </span>
             </div>
             <div className="flex flex-col animate-pulse space-y-2.5 w-full">
               <div className="flex items-center w-1/2">
