4ebb4d0a44831e6db0d246fab2c7d47c0a19ec4d
kienvc <17985897+kienvc@users.noreply.github.com>
2025-01-02 22:57:23 +0700

    fix syntax

--- app/api/lib/db.ts
+++ app/api/lib/db.ts
@@ -6,6 +6,7 @@ if (!global._pgPool) {
   const {serverRuntimeConfig} = getConfig();
   global._pgPool = new Pool({
     connectionString: serverRuntimeConfig.POSTGRES_URL,
+  });
 }
 
 pool = global._pgPool;
