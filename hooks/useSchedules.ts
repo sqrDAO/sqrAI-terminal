a4e843fb79bacf1f31b08a1b548754a7e9d48781
CloudCludfore <CloudCludfore@gmail.com>
2025-01-13 11:13:58 +0700

    commit

--- hooks/useSchedules.ts
+++ hooks/useSchedules.ts
@@ -2,5 +2,14 @@ import { getSchedules } from "@/app/serivces/agent.service";
 import { useQuery } from "@tanstack/react-query";
 
 export function useSchedules(agentId: string, publicKey: string) {
-  return useQuery({ queryKey: ["agents", {agentId, publicKey}], queryFn: () => getSchedules(agentId, publicKey) });
+  return useQuery({
+    queryKey: ["agents", { agentId, publicKey }],
+    queryFn: () => {
+      if (agentId && publicKey) {
+        return getSchedules(agentId, publicKey);
+      }
+      return Promise.reject(new Error("Missing agentId or publicKey"));
+    },
+    enabled: !!agentId && !!publicKey,
+  });
 }
