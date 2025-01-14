b4440e0e4a748ed53275c1cf4d6685a7e1601327
sonln99 <sonln4793@gmail.com>
2025-01-14 14:47:58 +0700

    update zoom in out chat box

--- app/(sidebar)/layout.tsx
+++ app/(sidebar)/layout.tsx
@@ -1,9 +1,8 @@
 "use client";
-import type { Metadata } from "next";
-import { Bricolage_Grotesque, Chakra_Petch } from "next/font/google";
 import Sidebar from "./sidebar";
 import AiChat from "./chat/Index";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { Button } from "@/components/ui/button";
 
 // export const metadata: Metadata = {
 //   title: "Overview",
@@ -15,12 +14,67 @@ export default function SidebarLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [isExpanded, setIsExpanded] = useState(false);
+  useEffect(() => {
+    const chatContainer = document.getElementById("chat-container");
+    if (chatContainer) {
+      chatContainer.style.width = isExpanded ? "100vw" : "375px";
+    }
+  }, [isExpanded]);
   return (
     <div className={`w-full flex border-t border-t-[#DCFF9F]`}>
       <Sidebar />
       {children}
-      <div className="w-[375px] min-w-[375px] h-[calc(100vh_-77px)]">
+      <div
+        className="w-[375px] min-w-[375px] h-[calc(100vh_-77px)] relative"
+        id="chat-container"
+      >
         <AiChat />
+
+        <Button
+          className="absolute -left-9 bottom-12 w-1 z-50"
+          onClick={() => {
+            setIsExpanded(!isExpanded);
+          }}
+        >
+          {isExpanded ? (
+            <svg
+              className="w-6 h-6 text-gray-800 dark:text-white"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="m10 16 4-4-4-4"
+              />
+            </svg>
+          ) : (
+            <svg
+              className="w-6 h-6 text-gray-800 dark:text-white"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="m14 8-4 4 4 4"
+              />
+            </svg>
+          )}
+        </Button>
       </div>
     </div>
   );
